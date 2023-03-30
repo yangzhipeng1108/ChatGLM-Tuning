dd483740a255c111373434078a27eb3f5c0490ef
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-03-30 16:46:18 +0800

    Create finetune-multi-gpu.py

---
+++ finetune-multi-gpu.py
@@ -0,0 +1,180 @@
+from transformers import TrainingArguments
+from transformers import Trainer, HfArgumentParser
+from transformers import AutoTokenizer
+from modeling_chatglm import ChatGLMForConditionalGeneration
+import torch
+import torch.nn as nn
+from peft import get_peft_model, LoraConfig, TaskType
+from dataclasses import dataclass, field
+import datasets
+import os
+
+
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+
+
+@dataclass
+class FinetuneArguments:
+    dataset_path: str = field(default="data/alpaca")
+    model_path: str = field(default="output")
+    lora_rank: int = field(default=8)
+
+
+class CastOutputToFloat(nn.Sequential):
+    def forward(self, x):
+        return super().forward(x).to(torch.float32)
+
+
+def get_masks_and_position_ids(
+    seq, seq_len, context_length, device, gmask=False, position_encoding_2d=True
+):
+    mask_position = (
+        seq_len - 2
+    )  # is equal to `seq.index(mask_token)` or `seq.index(150001)`
+    attention_mask = torch.ones((1, context_length, context_length), device=device)
+    attention_mask.tril_()
+    attention_mask[..., : mask_position - 1] = 1
+    attention_mask = (attention_mask < 0.5).bool()
+
+    if position_encoding_2d:
+        seq_length = seq_len - 1  # is equal to `seq_length = seq.index(150004)`
+        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
+        if not gmask:
+            position_ids[seq_length:] = mask_position
+        block_position_ids = torch.cat(
+            (
+                torch.zeros(seq_length, dtype=torch.long, device=device),
+                torch.arange(
+                    context_length - seq_length, dtype=torch.long, device=device
+                )
+                + 1,
+            )
+        )
+        position_ids = torch.stack((position_ids, block_position_ids), dim=0)
+    else:
+        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
+        if not gmask:
+            position_ids[context_length - 1 :] = mask_position
+    return attention_mask, position_ids
+
+
+def data_collator(features: list) -> dict:
+    len_ids = [len(feature["input_ids"]) for feature in features]
+    longest = max(len_ids)
+    input_ids = []
+    attention_mask_list = []
+    position_ids_list = []
+    labels_list = []
+    for ids_l, feature in sorted(zip(len_ids, features), key=lambda x: -x[0]):
+        ids = feature["input_ids"]
+        seq_len = feature["seq_len"]
+        labels = (
+            [-100] * (seq_len - 1)
+            + ids[(seq_len - 1) :]
+            + [-100] * (longest - ids_l)
+        )
+        ids = ids + [tokenizer.pad_token_id] * (longest - ids_l)
+        _ids = torch.LongTensor(ids)
+        attention_mask, position_ids = get_masks_and_position_ids(
+            ids, seq_len, longest, _ids.device, gmask=False
+        )
+        labels_list.append(torch.LongTensor(labels))
+        input_ids.append(_ids)
+        attention_mask_list.append(attention_mask)
+        position_ids_list.append(position_ids)
+    input_ids = torch.stack(input_ids)
+    labels = torch.stack(labels_list)
+    attention_mask = torch.stack(attention_mask_list)
+    position_ids = torch.stack(position_ids_list)
+    return {
+        "input_ids": input_ids,
+        "labels": labels,
+        "attention_mask": attention_mask,
+        "position_ids": position_ids,
+    }
+
+
+class ModifiedTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        return model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            position_ids=inputs["position_ids"],
+            labels=inputs["labels"],
+        ).loss
+
+    def save_model(self, output_dir=None, _internal_call=False):
+        from transformers.trainer import TRAINING_ARGS_NAME
+        os.makedirs(output_dir, exist_ok=True)
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
+        saved_params = {
+            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
+        }
+        torch.save(saved_params, os.path.join(output_dir, "adapter_model.bin"))
+
+
+def main():
+    finetune_args, training_args = HfArgumentParser(
+        (FinetuneArguments, TrainingArguments)
+    ).parse_args_into_dataclasses()
+    
+    device_map = "auto"
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    ddp = world_size != 1
+    if ddp:
+        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+
+    # init model
+    model = ChatGLMForConditionalGeneration.from_pretrained(
+        "THUDM/chatglm-6b", load_in_8bit=True,  device_map=device_map
+    )
+    #model.gradient_checkpointing_enable()
+    model.enable_input_require_grads()
+    model.is_parallelizable = True
+    model.model_parallel = True
+    model.lm_head = CastOutputToFloat(model.lm_head)
+    model.config.use_cache = (
+        False  # silence the warnings. Please re-enable for inference!
+    )
+
+    # setup peft
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=finetune_args.lora_rank,
+        lora_alpha=32,
+        lora_dropout=0.1,
+    )
+    model = get_peft_model(model, peft_config)
+
+    # load dataset
+    dataset = datasets.load_from_disk(finetune_args.dataset_path).shuffle()
+
+    # start train
+    trainer = ModifiedTrainer(
+        model=model,
+        train_dataset=dataset,
+        args=training_args,
+        data_collator=data_collator,
+    )
+    trainer.train()
+
+    # save model
+    model.save_pretrained(training_args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+WORLD_SIZE=8 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 --master_port=3192 finetune.py \
+--dataset_path data/alpaca --lora_rank 8 --per_device_train_batch_size 4 --gradient_accumulation_steps 1 \
+ --save_steps 1000 --save_total_limit 2 --learning_rate 1e-4 --fp16 \
+ --remove_unused_columns false --logging_steps 50 --output_dir output
+ 
+ WORLD_SIZE=8 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node=8 --master_port=3192 finetune.py \
+--dataset_path data/alpaca --lora_rank 8 --per_device_train_batch_size 4 --gradient_accumulation_steps 32 \
+ --save_steps 1000 --save_total_limit 2 --learning_rate 1e-4 --fp16 \
+ --remove_unused_columns false --logging_steps 50 --output_dir output
+'''
